c2b83e57b28f32a5c1114b99e369b381f0608d8c
YushenHu0326 <62900433+YushenHu0326@users.noreply.github.com>
2024-03-21 20:09:17 -0400

    Update guitar.m

--- guitar.m
+++ guitar.m
@@ -6,7 +6,25 @@ function [x]=gdist(a,x)
 end
 
 clear all
-f1=440; % frequency in Hz (cycles/second)
+
+%frequencies for all 6 strings
+f=[82,110,147,196,247,330];
+f1=f(1); f2=f(2); f3=f(3); f4=f(4); f5=f(5); f6=f(6);
+%fret distance chart
+fret=zeros(1,24);
+fret(1)=0.056125;        fret(2)=0.10910185;
+fret(3)=0.1591034;       fret(4)=0.20629938;
+fret(5)=0.25084722;      fret(6)=0.29289352;
+fret(7)=0.33258024691;   fret(8)=0.37004012345;
+fret(9)=0.40539660493;   fret(10)=0.43876851851;
+fret(11)=0.47026851851;  fret(12)=0.5;
+fret(13)=0.5280632716;   fret(14)=0.55455092592;
+fret(15)=0.57955246913;  fret(16)=0.60314969135;
+fret(17)=0.6254228395;   fret(18)=0.64644598765;
+fret(19)=0.66629012345;  fret(20)=0.68502006172;
+fret(21)=0.70269753086;  fret(22)=0.71938425925;
+fret(23)=0.73513425925;  fret(24)=0.75;
+
 %string parameters to make frequency f1:
 L=1;
 M=1;
@@ -23,12 +41,17 @@ dtmax=-(R/T)+sqrt((R/T)^2+(dx^2/(T/M)));
 %Also, make nskip as small as possible, given the above criteria.
 nskip=ceil(1/(8192*dtmax));
 dt=1/(8192*nskip);
-tmax=4; %total time of the simulation in seconds
+tmax=10; %total time of the simulation in seconds
 clockmax=ceil(tmax/dt);
+
+%Initialize pickup position
+pickup = 0.9;
+
 %initial conditions for plucked string:
 V=zeros(1,J);
-xp=L/3;
+xp=L*pickup;
 Hp=1; %position and amplitude of pluck
+
 for jj=1:J
     x=(jj-1)*dx;
     if(x<xp)
@@ -38,10 +61,12 @@ for jj=1:J
     end
 end
 
+%plot(H)
+
 drawnow
 count=0;
 S=zeros(1,ceil(clockmax/nskip));
-tsave = zeros(1,ceil(clockmax/nskip));
+tsave=zeros(1,ceil(clockmax/nskip));
 j=2:(J-1); % list of indices of interior points
 for clock=1:clockmax
     t=clock*dt;
@@ -49,7 +74,7 @@ for clock=1:clockmax
     H(j)=H(j)+dt*V(j);
     if(mod(clock,nskip)==0)
         count=count+1;
-        S(count)=H(2); %sample the sound
+        S(count)=H(ceil(J*pickup)); %sample the sound
         tsave(count)=t; %record sample time
     end
     %set(Hhandle,'ydata',H) %update movie frame
