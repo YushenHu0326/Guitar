fa1878ea1df8565171318a0ea5250ce982645d1d
70sephX <104687808+70sephX@users.noreply.github.com>
2024-04-10 18:27:02 -0400

    Update wah function

--- guitar.m
+++ guitar.m
@@ -467,9 +467,47 @@ for clock=1:clockmax
     %drawnow %show latest frame
 end
 
+function wah(audio_in, fs)
+    % Input: audio file
+    damping = 0.06; %The lower it is, the smaller pass band
+    width = 500;
+    min_cutoff = 500; % Minimum cut off frequency 
+    max_cutoff = 3000;% Maximum cut off frequency
+    center_freq = width/fs;
+    cutoff_freq=min_cutoff:center_freq:max_cutoff;
+    while(length(cutoff_freq) < length(audio_in) )
+        cutoff_freq = [ cutoff_freq (max_cutoff:-center_freq:min_cutoff) ];
+        cutoff_freq = [ cutoff_freq (min_cutoff:center_freq:max_cutoff) ];
+    end
+    cutoff_freq = cutoff_freq(1:length(audio_in));
+    % control the center frequency
+    F1 = 2*sin((pi*cutoff_freq(1))/fs);
+    Q1 = 2*damping;
+    % Create and Zero Vectors to Match Length of Audio Input File
+    highpass=zeros(size(audio_in));
+    bandpass=zeros(size(audio_in));
+    lowpass=zeros(size(audio_in));
+    highpass(1) = audio_in(1);
+    bandpass(1) = F1*highpass(1);
+    lowpass(1) = F1*bandpass(1);
+    for n=2:length(audio_in)
+        highpass(n) = audio_in(n) - lowpass(n-1) - Q1*bandpass(n-1);
+        bandpass(n) = F1*highpass(n) + bandpass(n-1);
+        lowpass(n) = F1*bandpass(n) + lowpass(n-1);
+        F1 = 2*sin((pi*cutoff_freq(n))/fs);
+    end
+    % Normalize and play back
+    normed = bandpass./max(max(abs(bandpass)));
+    audiowrite('wah wahed.wav', normed, fs);
+    sound(normed, fs);
+end
+
 S=gdist(0.9995,S);
 
-soundsc(S(1:count))
+%soundsc(S(1:count))
+
+wah(S(1:count),8192)
+
 %plot the soundwave as a function of time:
 %figure
 plot(tsave(1:count),S(1,1:count))
